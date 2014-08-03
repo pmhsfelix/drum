d2308fa8207cfe26564d42d3fb384a15b2468049
Pedro Felix <pmhsfelix@gmail.com>
2014-08-03 18:19:44 +0100

    Copies and renames the UriFactory from the Web API workshop demos

---
+++ src/Drum.Tests/UriMakerTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using Xunit;
+
+namespace Drum.Tests
+{
+    public class UriMakerTests
+    {
+        [RoutePrefix("api/resources")]
+        public class ResourceController : ApiController
+        {
+            [Route("", Name = "GetAll")]
+            public void GetAll() { }
+
+            [Route("", Name = "GetAllPaged")]
+            public void GetPaged(int page, int count) { }
+
+            [Route("{id}", Name = "GetById")]
+            public void GetById(int id) { }
+
+            [Route("", Name = "GetByProp1")]
+            public void GetByProp1(string prop1) { }
+
+            [Route("", Name = "GetByProp2")]
+            public void GetByProp2(string prop2) { }
+
+        }
+        
+        [Fact]
+        public void Fact()
+        {
+            var uri = _uriMaker.UriFor(c => c.GetAll());
+            Assert.Equal("http://example.org/api/resources", uri.ToString());
+        }
+
+        public UriMakerTests()
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.EnsureInitialized();
+            var req = new HttpRequestMessage(HttpMethod.Get,"http://example.org");
+            req.SetConfiguration(config);
+            _uriMaker = new UriMaker<ResourceController>(new UrlHelper(req));
+        }
+
+        private readonly UriMaker<ResourceController> _uriMaker;
+    }
+}
---
+++ src/Drum/UriMaker.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace Drum
+{
+    public class UriMaker
+    {
+        private readonly UrlHelper _urlHelper;
+
+        private static readonly MethodInfo DictionaryAddMethod = typeof(Dictionary<string, object>).GetMethod(
+            "Add",
+            new Type[] { typeof(string), typeof(object) });
+
+        public UriMaker(UrlHelper urlHelper)
+        {
+            _urlHelper = urlHelper;
+
+        }
+
+        public Uri UriFor<T>(Expression<Action<T>> action)
+        {
+            var methodCall = action.Body as MethodCallExpression;
+            if (methodCall == null)
+            {
+                throw new ArgumentException("The expression must be a method call.", "method");
+            }
+
+            var actionMethod = methodCall.Method;
+            var actionParameters = actionMethod.GetParameters();
+
+            // Get the route
+            var route = actionMethod.GetCustomAttributes(typeof(RouteAttribute), true)
+                .Cast<RouteAttribute>()
+                .FirstOrDefault(r => !String.IsNullOrEmpty(r.Name));
+            if (route == null)
+            {
+                throw new InvalidOperationException("Action method must have a named Route attribute");
+            }
+
+            if (methodCall.Arguments.Count == 0)
+            {
+                return new Uri(_urlHelper.Link(route.Name, new { }));
+            }
+
+            // Build an expression that creates a dictionary with the following structure:
+            //
+            // { <actionParamaterName>, <actionParameterValue> }
+            var routeValuesExpr = Expression.Lambda<Func<Dictionary<string, object>>>
+                (
+                    Expression.ListInit(
+                        Expression.New(typeof(Dictionary<string, object>)),
+                        methodCall.Arguments.Select((a, i) => Expression.ElementInit(
+                            DictionaryAddMethod,
+                            Expression.Constant(actionParameters[i].Name),
+                            Expression.Convert(a, typeof(object))))
+                        )
+                );
+
+            // Compile and evaluate the expressions
+            var routeValuesGetter = routeValuesExpr.Compile();
+            var routeValues = routeValuesGetter();
+
+            return new Uri(_urlHelper.Link(route.Name, routeValues));
+        }
+    }
+
+    public class UriMaker<TController>
+    {
+        private readonly UriMaker _uriMaker;
+
+        public Uri UriFor(Expression<Action<TController>> action)
+        {
+            return _uriMaker.UriFor(action);
+        }
+
+        public UriMaker(UrlHelper urlHelper)
+        {
+            _uriMaker = new UriMaker(urlHelper);
+        }
+    }
+}
